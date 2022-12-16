9505284440184165a4ead619100dfcced047a48a
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-12-15 21:56:57 -0500

    modeled the data, setted up api routes to work with that data, test the routes to make sure evrythine sure everything works as intended, repeated with new model

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS just_tech_news_db;
+
+CREATE DATABASE just_tech_news_db;
+
+INSERT INTO users
+  (username, email, password)
+VALUES
+  ("Lernantino", "lernantino@gmail.com", "password1234");
